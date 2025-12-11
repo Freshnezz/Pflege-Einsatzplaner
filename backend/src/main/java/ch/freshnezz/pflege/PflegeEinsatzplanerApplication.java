cf2b570a6b1a96ad3fb136c3392b65246e7de3ff
Freshnezz <marc.augsburger@gmail.com>
2025-12-11 09:07:29 +0100

    feat: add spring Boot b ackend base project

---
+++ backend/src/main/java/ch/freshnezz/pflege/PflegeEinsatzplanerApplication.java
@@ -0,0 +1,13 @@
+package ch.freshnezz.pflege;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PflegeEinsatzplanerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PflegeEinsatzplanerApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/ch/freshnezz/pflege/PflegeEinsatzplanerApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.freshnezz.pflege;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PflegeEinsatzplanerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
